98232180530ddce8cb4edf9b62239d331f27d107
Luís Chula <l.chula@campus.fct.unl.pt>
2021-03-17 17:08:09 +0000

    ex 3.1

--- P1/MonteCarloPI.java
+++ P1/MonteCarloPI.java
@@ -9,19 +9,27 @@ public class MonteCarloPI {
     }
 
     private static boolean isInTheCircle(double x, double y){
-        System.out.println("("+x+","+y+")");
         return (Math.pow(x, 2) + Math.pow(y, 2) < 1) ? true : false;
+    }
+
+    private static void print(int nPoints, int within, double pi){
+        System.out.println("Total Number of points: " + nPoints);
+        System.out.println("Points within circle: " + within);
+        System.out.println("Pi estimation: " + pi);
     }
 
     public static void main(String[] args) {
         int nPoints = Integer.parseInt(args[0]);
-        int m = 0;
+        int within = 0;
         for(int i = 0; i < nPoints; i++){
             double x = randInt(-1, 1);
             double y = randInt(-1, 1);
             if(isInTheCircle(x, y))
-                m++;
+                within++;
         }
+        double pi = 4.0*((double)within/(double)nPoints);
+
+        print(nPoints, within, pi);
         
     } 
     
