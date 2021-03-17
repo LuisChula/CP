4d4288c5af6809bed53202299fef1aa0fcba1a0d
Luís Chula <l.chula@campus.fct.unl.pt>
2021-03-17 20:32:06 +0000

    threads working but still have to improve the counting

--- P1/Main.java
+++ P1/Main.java
@@ -1,7 +1,36 @@
 public class Main {
 
     public static void main( String args[] ) {
-        
+
+        int nPoints = Integer.parseInt(args[0]);  
+        int nThreads = 1;
+        if(args.length > 1 && args[1] != "0") {
+            nThreads = Integer.parseInt(args[1]);
+        }
+
+        int m = nPoints / nThreads;
+        MonteCarloPI[] mcps = new MonteCarloPI[nThreads];
+        Thread[] ts = new Thread[nThreads];
+        for(int i = 0; i < nThreads; i++){
+            MonteCarloPI mcp = new MonteCarloPI(m);
+            Thread t = new Thread(mcp);
+            t.start();
+            mcps[i] = mcp;
+            ts[i] = t;
+        }
+
+        int within = 0;
+        for(int i = 0; i < nThreads; i++) {
+            try{
+                ts[i].join();
+                within += mcps[i].getWithin();
+            } catch(Exception e){
+                System.out.println(e);
+            }
+        }
+
+        double pi = 4.0*((double)within/(double)nPoints);
+        MonteCarloPI.print(nPoints, within, pi);
     }
 
 }
