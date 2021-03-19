bafc90f26922a9ef05caeebcc8789ad7349c6e1c
Luís Chula <l.chula@campus.fct.unl.pt>
2021-03-19 18:59:17 +0000

    final ex1

--- P1/MonteCarloPI.java
+++ P1/MonteCarloPI.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 public class MonteCarloPI {
 
-    private static double randInt(int rangeMin, int rangeMax) {
-        Random r = new Random();
-        double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
-        return randomValue;
-    }
-
     private static boolean isInTheCircle(double x, double y){
         return (Math.pow(x, 2) + Math.pow(y, 2) < 1) ? true : false;
     }
@@ -20,13 +14,15 @@ public class MonteCarloPI {
 
     public static void main(String[] args) {
         int nPoints = Integer.parseInt(args[0]);
+        Random r = new Random();
         int within = 0;
         for(int i = 0; i < nPoints; i++){
-            double x = randInt(-1, 1);
-            double y = randInt(-1, 1);
+            double x = r.nextDouble();
+            double y = r.nextDouble();
             if(isInTheCircle(x, y))
                 within++;
         }
+        
         double pi = 4.0*((double)within/(double)nPoints);
 
         print(nPoints, within, pi);
