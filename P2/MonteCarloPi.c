a4d8372acfab39c4869178cdf29f911534deae5e
Luís Chula <l.chula@campus.fct.unl.pt>
2021-03-24 15:55:13 +0000

    thread example

--- P2/MonteCarloPi.c
+++ P2/MonteCarloPi.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <pthread.h>
 
-int main() {
-   printf("Hello, World!\n");
+// A normal C function that is executed as a thread
+// when its name is specified in pthread_create()
+void* monteCarloPi(void *arg)
+{
+   int v = *(int*)arg;
+   for(int i = 0; i < 100; i++){
+      printf("Thread %d => %d\n", v, i);
+   }
+	return NULL;
+}
+
+int main()
+{
+   int nThreads = 4;
+   int nRuns = 100;
+   int r = nRuns/nThreads;
+
+   pthread_t t[nThreads];
+   printf("Before Thread\n");
+   for(int i = 0; i < nThreads; i++){
+      pthread_create(&t[i], NULL, monteCarloPi, &i);
+   }
+
+   for(int i = 0; i < nThreads; i++){
+      pthread_join(t[i], NULL);
+   }
+	printf("After Thread\n");
+	
    return 0;
 }
